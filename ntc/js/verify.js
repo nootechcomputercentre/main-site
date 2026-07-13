ab00baddade053e65048c937dd4096097532d6a2
NOOTECH COMPUTER CENTRE <nootechcomputercentre@gmail.com>
2026-07-13 20:10:49 +0530

    Create verify.js

---
+++ ntc/js/verify.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL =
+"https://yzyupyrootanffzouker.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_FHztZiMDfLcDPaTnQqg51Q_p7K59PJ0";
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
