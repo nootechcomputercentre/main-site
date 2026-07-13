f416b6a9edd9f9e60e0f5c64cee5d0f10f6a38aa
NOOTECH COMPUTER CENTRE <nootechcomputercentre@gmail.com>
2026-07-13 22:35:37 +0530

    Update verify.js

--- ntc/js/verify.js
+++ ntc/js/verify.js
@@ -1,2 +1,74 @@
-alert("verify.js Loaded Successfully");
-console.log("verify.js Loaded Successfully");
+const SUPABASE_URL ="https://yzyupyrootanffzouker.supabase.co";
+const SUPABASE_KEY ="sb_publishable_FHztZiMDfLcDPaTnQqg51Q_p7K59PJ0";
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+async function verifyCertificate() {
+
+    const params = new URLSearchParams(window.location.search);
+
+    const hash = params.get("id");
+
+    if (!hash) {
+
+        document.getElementById("loading").style.display = "none";
+        document.getElementById("error").style.display = "block";
+        return;
+
+    }
+
+    const { data, error } = await supabase
+        .from("certificates")
+        .select("*")
+        .eq("certificate_hash", hash)
+        .single();
+
+    document.getElementById("loading").style.display = "none";
+
+    if (error || !data) {
+
+        document.getElementById("error").style.display = "block";
+        return;
+
+    }
+
+    document.getElementById("result").style.display = "block";
+
+    document.getElementById("status").innerHTML =
+        "✅ CERTIFICATE VERIFIED";
+
+    document.getElementById("certificate_no").innerHTML =
+        data.certificate_no;
+
+    document.getElementById("student_name").innerHTML =
+        data.student_name;
+
+    document.getElementById("father_name").innerHTML =
+        data.father_name;
+
+    document.getElementById("course_name").innerHTML =
+        data.course_name;
+
+    document.getElementById("batch_name").innerHTML =
+        data.batch_name;
+
+    document.getElementById("percentage").innerHTML =
+        data.percentage;
+
+    document.getElementById("grade").innerHTML =
+        data.grade;
+
+    document.getElementById("result_status").innerHTML =
+        data.result;
+
+    document.getElementById("issue_date").innerHTML =
+        data.issue_date;
+
+    document.getElementById("verification_status").innerHTML =
+        data.verification_status;
+
+}
+
+verifyCertificate();
