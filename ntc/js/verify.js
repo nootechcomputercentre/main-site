408ef4c1f638f0a39eecb25e0525dab47d12dbd3
NOOTECH COMPUTER CENTRE <nootechcomputercentre@gmail.com>
2026-07-13 23:32:08 +0530

    Update verify.js

--- ntc/js/verify.js
+++ ntc/js/verify.js
@@ -1,75 +1,27 @@
+
+console.clear();
+
+console.log("STARTED");
+
 const SUPABASE_URL = "https://yzyupyrootanffzouker.supabase.co";
 const SUPABASE_KEY = "sb_publishable_FHztZiMDfLcDPaTnQqg51Q_p7K59PJ0";
-console.log("verify.js started");
-const supabase = window.supabase.createClient(
+
+const client = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
 
-async function verifyCertificate() {
+console.log("CLIENT CREATED");
 
-    const params = new URLSearchParams(window.location.search);
+(async () => {
 
-    const hash = params.get("id");
+    console.log("CHECKING DATABASE");
 
-    if (!hash) {
-
-        document.getElementById("loading").style.display = "none";
-        document.getElementById("error").style.display = "block";
-        return;
-
-    }
-
-    const { data, error } = await supabase
+    const { data, error } = await client
         .from("certificates")
-        .select("*")
-        .eq("certificate_hash", hash)
-        .single();
+        .select("*");
 
-    document.getElementById("loading").style.display = "none";
+    console.log(data);
+    console.log(error);
 
-    if (error || !data) {
-
-        document.getElementById("error").style.display = "block";
-        return;
-
-    }
-
-    document.getElementById("result").style.display = "block";
-
-    document.getElementById("status").innerHTML =
-        "✅ CERTIFICATE VERIFIED";
-
-    document.getElementById("certificate_no").innerHTML =
-        data.certificate_no;
-
-    document.getElementById("student_name").innerHTML =
-        data.student_name;
-
-    document.getElementById("father_name").innerHTML =
-        data.father_name;
-
-    document.getElementById("course_name").innerHTML =
-        data.course_name;
-
-    document.getElementById("batch_name").innerHTML =
-        data.batch_name;
-
-    document.getElementById("percentage").innerHTML =
-        data.percentage;
-
-    document.getElementById("grade").innerHTML =
-        data.grade;
-
-    document.getElementById("result_status").innerHTML =
-        data.result;
-
-    document.getElementById("issue_date").innerHTML =
-        data.issue_date;
-
-    document.getElementById("verification_status").innerHTML =
-        data.verification_status;
-
-}
-
-verifyCertificate();
+})();
